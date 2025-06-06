24845018dd68d536b4c89f9b022453c0e058456e
Yuriy Stul <ysden123@gmail.com>
2025-06-06 19:47:44 +0300

    1st examples for serilog.

---
+++ PSerilog/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using Serilog.Events;
+namespace PSerilog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogEventLevel minLevel;
+#if DEBUG
+            Console.WriteLine("DEBUG is defined");
+            minLevel = LogEventLevel.Debug;
+#else
+            Console.WriteLine("No DEBUG is defined");
+            minLevel = LogEventLevel.Error;
+#endif
+            // Create loggers Console and File. These loggers will be used globally.
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}",
+                restrictedToMinimumLevel: minLevel)
+            .WriteTo.File("logs/Logging.log",
+                rollingInterval: RollingInterval.Day,
+                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}",
+                restrictedToMinimumLevel: minLevel)
+            .CreateLogger();
+
+            Console.WriteLine("Hello!");
+
+            Log.Information("Entering the application");
+
+            var class1 = new Class1();
+            class1.Method1();
+
+            Log.Error("Some test error log");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DotNetLogging
 Playing with some logging packets for projects on C#.
+
+## PSerilog
+Examples of using the Serilog package.
