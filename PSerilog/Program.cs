d91f9bfc0d15535e2dde74105d3d2dee46ca0cb9
Yuriy Stul <ysden123@gmail.com>
2025-06-14 19:24:17 +0300

    Added thread ID into log.

--- PSerilog/Program.cs
+++ PSerilog/Program.cs
@@ -6,24 +6,25 @@ namespace PSerilog
     {
         static void Main(string[] args)
         {
-            LogEventLevel minLevel;
 #if DEBUG
-            Console.WriteLine("DEBUG is defined");
-            minLevel = LogEventLevel.Debug;
-#else
-            Console.WriteLine("No DEBUG is defined");
-            minLevel = LogEventLevel.Error;
-#endif
-            // Create loggers Console and File. These loggers will be used globally.
             Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}",
-                restrictedToMinimumLevel: minLevel)
-            .WriteTo.File("logs/Logging.log",
-                rollingInterval: RollingInterval.Day,
-                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}",
-                restrictedToMinimumLevel: minLevel)
-            .CreateLogger();
-
+               .MinimumLevel.Debug()
+               .Enrich.With(new ThreadIdEnricher())
+               .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} [{ThreadId}] {Message:lj}{NewLine}{Exception}")
+               .WriteTo.File("logs/PSerilog.log",
+               rollingInterval: RollingInterval.Day,
+               outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} [{ThreadId}] {Message:lj}{NewLine}{Exception}")
+           .CreateLogger();
+#else
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Error()
+               .Enrich.With(new ThreadIdEnricher())
+               .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} [{ThreadId}] {Message:lj}{NewLine}{Exception}")
+               .WriteTo.File("logs/PSerilog.log",
+               rollingInterval: RollingInterval.Day,
+               outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} [{ThreadId}] {Message:lj}{NewLine}{Exception}")
+           .CreateLogger();
+#endif
             Console.WriteLine("Hello!");
 
             Log.Information("Entering the application");
---
+++ PSerilog/ThreadIdEnricher.cs
@@ -0,0 +1,14 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace PSerilog
+{
+    internal class ThreadIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                    "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+}
