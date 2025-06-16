5c675f2b12a7911abe4590a32a6297cdf14be7f2
Yuriy Stul <ysden123@gmail.com>
2025-06-16 12:35:56 +0300

    Serilog without initialization.

---
+++ PSerilogWOInit/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace PSerilogWOInit
+{
+    internal class Class1
+    {
+        /// <summary>
+        /// The logger for the class.
+        /// </summary>
+        private readonly ILogger _logger;
+
+        public Class1()
+        {
+            // Create a logger for a specific class.
+            _logger = Log.ForContext<Class1>();
+            Console.WriteLine($"_logger: {_logger}");
+        }
+
+        public void Method1()
+        {
+            _logger.Information("==>Class1.Method1");
+            _logger.Debug("Some debug output: {Val}", 123);
+        }
+    }
+}
---
+++ PSerilogWOInit/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+
+namespace PSerilogWOInit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                Console.WriteLine("Hello!");
+
+                Log.Information("Entering the application");
+
+                var class1 = new Class1();
+                class1.Method1();
+
+                Log.Error("Some test error log");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
